2f50f952ed87c5e221bc4ed2001a3d160e54a34b
minnja6 <jasmine.minniweather@verizon.com>
2019-08-01 13:40:01 -0500

    updates to photocontainer

--- src/Photo.js
+++ src/Photo.js
@@ -1,8 +1,9 @@
 import React from 'react';
+//import { tsConstructorType } from '@babel/types';
 const Photo = (props) => {
     return (
         <li>
-            <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
+            <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" key= {1}/>
         </li>
     );
 }
--- src/PhotoContainer.js
+++ src/PhotoContainer.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import NotFound from './NotFound';
 import Photo from './Photo';
-const PhotoContainer = (props) => {
-    return (
-        <div class="photo-container">
-            <h2>Results</h2>
-            <ul>
-                <Photo />
-                <Photo />
-                <Photo />
-                <Photo />
-                <Photo />
-                if(){
+class PhotoContainer extends React.Component {
+    state = {
+        Photo: 0
+    };
+    render() {
+        return (
+            <div className="photo-container">
+                <h2>Results</h2>
+                <ul>
+                    <Photo />
                     <NotFound />
-                }
-            </ul>
-        </div>
-    )
-};
+
+                </ul>
+            </div>
+        )
+    };
+}
 export default PhotoContainer;
